faa951a96292288fc4404c8aeb9dcdd795277592
Taylor Walla <taylor.j.walla@gmail.com>
2017-11-30 03:04:28 -0600

    use Brewfile, auto-install brew and be clever about it. add license and readme

--- brew.sh
+++ brew.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
+# Check if Homebrew is installed. If not, install it.
+if ! [ -x "$(command -v brew)" ]; then
+  echo "Homebrew is not installed. Installing it now..."
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "Homebrew is already installed, proceeding..."
+fi
 
 # Make sure we’re using the latest Homebrew.
 brew update
@@ -8,21 +14,8 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade
 
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
-brew install bash-completion2
+# Install everything from Brewfile
+brew bundle
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
@@ -30,53 +23,10 @@ if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
   chsh -s /usr/local/bin/bash;
 fi;
 
-# Install cask apps 
-brew cask install iterm2
-brew cask install alfred
-brew cask install keybase
-brew cask install postman
-brew cask install visual-studio-code
-brew cask install discord
-brew cask install virtualbox
-brew cask install steam
-brew cask install deluge
-brew cask install slack
-brew cask install synergy
-brew install irssi
-brew install ranger
-brew install stow
-
-# Install `wget` with IRI support.
-brew install wget --with-iri
-
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
-
-# Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
-brew install grep
-brew install openssh
-brew install screen
-
-# Install other useful binaries.
-brew install ack
-brew install atop
-brew install htop
-brew install git
-brew install git-lfs
-brew install imagemagick --with-webp
-brew install lua
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rlwrap
-brew install ssh-copy-id
-brew install tree
-brew install nmap
-brew install mtr
-brew install iftop
-
 # Cleanup
 brew cleanup --force
 rm -f -r /Library/Caches/Homebrew/*
+
+###############################################################################
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`. #
+###############################################################################
